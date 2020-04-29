6ed0bb719ec3edc79d13b365446ab74a5011a89e
Bill <billcshto@gmail.com>
2020-04-29 19:08:56 -0400

    add: implement course_planner_api serializers

--- course_planner/settings.py
+++ course_planner/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 
 import os
 
-from course_planner_secrets.secrets_file import *
+from .course_planner_secrets.secrets_file import *
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
---
+++ course_planner_api/serializers.py
@@ -0,0 +1,57 @@
+from rest_framework import serializers
+
+from course_planner_api import models
+
+
+class UserProfileSerializer(serializers.ModelSerializer):
+    """Serializes a user profile"""
+
+    class Meta:
+        model = models.UserProfile
+        fields = (
+            "id",
+            "email",
+            "name",
+            "password",
+            "required_courses",
+            "current_academic_term",
+        )
+        extra_kwargs = {
+            "password": {"write_only": True, "style": {"input_type": "password"}}
+        }
+
+    def create(self, validated_data):
+        """Create and return a new user"""
+        user = models.UserProfile.objects.create_user(
+            email=validated_data["email"],
+            name=validated_data["name"],
+            password=validated_data["password"],
+        )
+
+        return user
+
+    def update(self, instance, validated_data):
+        """Handle updating user account"""
+        if "password" in validated_data:
+            password = validated_data.pop("password")
+            instance.set_password(password)
+
+        return super().update(instance, validated_data)
+
+
+class CourseItemSerializer(serializers.ModelSerializer):
+    """Serializers course information"""
+
+    class Meta:
+        model = models.CourseItem
+        fields = (
+            "id",
+            "user_profile",
+            "course_subject",
+            "course_number",
+            "semester_placement",
+            "year_placement",
+            "reqs",
+            "semester_offered",
+        )
+        extra_kwargs = {"user_profile": {"read_only": True}}
