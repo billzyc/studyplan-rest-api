5ce89d07c97880d67c8c9fccbe9ce91d1270ab59
Bill <billcshto@gmail.com>
2021-01-10 21:54:37 -0500

    update to postgresql

--- course_planner/settings.py
+++ course_planner/settings.py
@@ -72,14 +72,17 @@ TEMPLATES = [
 
 WSGI_APPLICATION = "course_planner.wsgi.application"
 
-
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": os.environ.get("NAME"),
+        "HOST": os.environ.get("HOST"),
+        "PORT": os.environ.get("PORT"),
+        "USER": os.environ.get("USER"),
+        "PASSWORD": os.environ.get("PASSWORD"),
     }
 }
 
